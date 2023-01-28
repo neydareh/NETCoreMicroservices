b814f3ebf8ad68ab9c410a3e4f13effc042c0b92
Olakunle Neye <olakunle.neye@thoughtworks.com>
2023-01-28 10:51:28 -0600

    formats doc use indent spacing of 2 instead of 4

--- Ecommerce.Api.Search/Services/CustomersService.cs
+++ Ecommerce.Api.Search/Services/CustomersService.cs
@@ -1,44 +1,51 @@
 ﻿using Ecommerce.Api.Search.Interfaces;
+using Ecommerce.Api.Search.Models;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Ecommerce.Api.Search.Services
 {
-    public class CustomersService : ICustomersService
+  public class CustomersService : ICustomersService
+  {
+    private readonly IHttpClientFactory httpClientFactory;
+    private readonly ILogger<ICustomersService> logger;
+
+    public CustomersService(IHttpClientFactory httpClientFactory, ILogger<ICustomersService> logger)
     {
-        private readonly IHttpClientFactory httpClientFactory;
-        private readonly ILogger<ICustomersService> logger;
-
-        public CustomersService(IHttpClientFactory httpClientFactory, ILogger<ICustomersService> logger)
-        {
-            this.httpClientFactory = httpClientFactory;
-            this.logger = logger;
-        }
-
-        public async Task<(bool IsSuccess, dynamic Customer, string ErrorMessage)> GetCustomerAsync(int id)
-        {
-            try
-            {
-                var client = httpClientFactory.CreateClient("CustomersService");
-                var response = await client.GetAsync($"api/customers/{id}");
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = await response.Content.ReadAsByteArrayAsync();
-                    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                    var result = JsonSerializer.Deserialize<dynamic>(content, options);
-
-                    return (true, result, null);
-                }
-                return (false, null, response.ReasonPhrase);
-            }
-            catch (Exception ex)
-            {
-                logger?.LogError(ex.ToString());
-                return (false, null, ex.Message);
-            }
-        }
+      this.httpClientFactory = httpClientFactory;
+      this.logger = logger;
     }
+
+    public async Task<(bool IsSuccess, Customer Customer, string ErrorMessage)> GetCustomerAsync(int id)
+    {
+      try
+      {
+        var client = httpClientFactory.CreateClient("CustomersService");
+        var response = await client.GetAsync($"api/customers/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+          var content = await response.Content.ReadAsByteArrayAsync();
+          var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+          var result = JsonSerializer.Deserialize<dynamic>(content, options);
+
+          return (true, result, null);
+        }
+        return (false, null, response.ReasonPhrase);
+      }
+      catch (Exception ex)
+      {
+        logger?.LogError(ex.ToString());
+        return (false, null, ex.Message);
+      }
+    }
+
+    public Task<(bool isSuccess, IEnumerable<Customer> Customers, string errorMessage)> GetCustomersAsync()
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
